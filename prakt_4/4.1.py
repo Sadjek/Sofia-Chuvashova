0d235f7c58cee71dee071b6fd2be6f5b50e9122a
Sadjek <sofiyachu@gmail.com>
2026-02-25 00:05:17 +0300

    Исправленный код 4.1 с добавленной валидацией

--- prakt_4/4.1.py
+++ prakt_4/4.1.py
@@ -1,6 +1,24 @@
-password = input("введите пароль")
-password_examination = input("введите пароль повтороно")
-if password == password_examination:
-    print ("пароль принят!")
+password = input("введите пароль ")
+password_examination = input("введите пароль повтороно ")
+a = "ABCDEFGHIJKLMNO"
+b = "1234567890"
+c = "@#$%^&*()_+{}[:]|<>?"
+cnt = 0
+a1 = False
+b1 = False
+c1 = False
+for i in range(0, len(password)):
+    if password[i] in a:
+        a1 = True
+    elif password[i] in b:
+        b1 = True
+    elif password[i] in c:
+        c1 = True
+
+if a1 and b1 and c1:
+    if password == password_examination:
+        print("пароль принят!")
+    else:
+        print("пароль не принят")
 else:
-    print("пароль не принят")
+    print('в пароле должны присутствовать заглавные буквы, цифры, спецсимволы')
